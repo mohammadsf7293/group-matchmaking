003dd0b81a42d5ed6290d781dbe1e85ac86de3da
DoubleAMJunior <aliabbasi1377@gmail.com>
2025-05-05 21:50:19 +0330

    Refactor: Extract SessionPlayer config from AppDbContext
    Moved the SessionPlayer table configuration from AppDbContext to a dedicated SessionPlayerConfiguration class for better structure and maintainability.

--- Rovio.MatchMaking.Repositories/Data/AppDbContext.cs
+++ Rovio.MatchMaking.Repositories/Data/AppDbContext.cs
@@ -14,34 +14,9 @@ namespace Rovio.MatchMaking.Repositories.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.ApplyConfiguration(new SessionConfiguration());
-
-
-            modelBuilder.Entity<SessionPlayer>(entity =>
-            {
-                entity.HasKey(e => e.Id); // Primary Key
-
-                entity.Property(e => e.SessionId)
-                      .IsRequired(); // Foreign key to Session
-
-                entity.Property(e => e.PlayerId)
-                      .IsRequired(); // Foreign key to Player
-
-                entity.Property(e => e.Status)
-                      .IsRequired()
-                      .HasDefaultValue("ATTENDED"); // Default Status
-
-                entity.Property(e => e.Score)
-                      .IsRequired()
-                      .HasDefaultValue(0); // Default Score
-/*
-                entity.Property(e => e.CreatedAt)
-                      .HasDefaultValueSql("CURRENT_TIMESTAMP"); // Default creation timestamp
-
-                entity.Property(e => e.UpdatedAt)
-                      .HasDefaultValueSql("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"); // Default updated timestamp
-*/
-            });
+            modelBuilder.ApplyConfiguration(new SessionPlayerConfiguration());
 
             modelBuilder.Entity<QueuedPlayer>()
                 .Property(e => e.Id)
---
+++ Rovio.MatchMaking.Repositories/Data/Configurations/SessionPlayerConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Rovio.MatchMaking.Repositories.Data.Configurations
+{
+    class SessionPlayerConfiguration : IEntityTypeConfiguration<SessionPlayer>
+    {
+        public void Configure(EntityTypeBuilder<SessionPlayer> entity)
+        {
+            entity.HasKey(e => e.Id); // Primary Key
+
+            entity.Property(e => e.SessionId)
+                  .IsRequired(); // Foreign key to Session
+
+            entity.Property(e => e.PlayerId)
+                  .IsRequired(); // Foreign key to Player
+
+            entity.Property(e => e.Status)
+                  .IsRequired()
+                  .HasDefaultValue("ATTENDED"); // Default Status
+
+            entity.Property(e => e.Score)
+                  .IsRequired()
+                  .HasDefaultValue(0); 
+        }
+    }
+}
