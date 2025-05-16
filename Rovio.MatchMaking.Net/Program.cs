0738de9e9fb7ac74270a0f3ad2b2fa047aa58a9f
DoubleAMJunior <aliabbasi1377@gmail.com>
2025-05-16 16:15:01 +0330

    Feature: MatchMaking.Net using env variables
    Updates program.cs from MatchMaking.Net to use Environment variables for configurations. Helps with setting up the container if not available it will fall back to previous behavior and it will not change any of the functionalities

--- Rovio.MatchMaking.Net/Program.cs
+++ Rovio.MatchMaking.Net/Program.cs
@@ -1,4 +1,4 @@
-using Autofac;
+﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Rovio.MatchMaking;
@@ -7,10 +7,14 @@ using Rovio.MatchMaking.Repositories;
 using Rovio.MatchMaking.Repositories.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
-// Register the DbContext with DI
+var connStr =  builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    options.UseMySql(connStr,
     new MySqlServerVersion(new Version(8, 0, 21))));
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
@@ -27,12 +31,19 @@ builder.Services.AddScoped<IQueuedPlayerRepository, QueuedPlayerRepository>();
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
-
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation(connStr);
+logger.LogInformation("Rovio.MatchMaking.Net has booted up at {Time}", DateTime.UtcNow);
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    logger.LogInformation("Started in development mode");
 }
 
+
+
 app.MapControllers();
-app.Run("http://localhost:5003");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5003";
+var host = Environment.GetEnvironmentVariable("HOST") ?? "localhost";
+app.Run($"http://{host}:{port}");
